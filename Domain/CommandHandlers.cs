9df7db7eb09dcd9e46e232a7cb263f11f6870d28
Thomas Jaeger <thomasjaeger@gmail.com>
2019-05-09 10:54:06 -0400

    Added additional comments

--- Domain/CommandHandlers.cs
+++ Domain/CommandHandlers.cs
@@ -31,17 +31,30 @@ namespace Domain
 
         private void Handle(SignUp cmd)
         {
+            // Create a new customer
             Customer customer = new Customer(cmd.GetMessageCreateOptions());
-            _customerRepository.Save(customer, customer.CustomerId.Id, -1);
+
+            // Commit the customer object's domain event(s) into the event store
+            _customerRepository.Save(customer, customer.CustomerId.Id, -1);  // Customer will be at version 0 after successfull commit into event store
         }
 
         private void Handle(ChargeCustomer cmd)
         {
+            // Get the customer from the event store
             Customer customer = _customerRepository.GetById(cmd.CustomerId.Id);
+
+            // Create proper currency value based on USD passed from the client
             Currency currency = Currency.ByAlphabeticCode(cmd.CurrencyCode);
-            int version = customer.Version;
+
+            int version = customer.Version;  // should be version 0 since we only created a customer
+
+            // Execute aggregate root method
             customer.Charge(new Money(cmd.Amount, currency), cmd.GetMessageCreateOptions());
+
+            // Commit all possible domain events created by customer
             _customerRepository.Save(customer, customer.CustomerId.Id, version);
+
+            // Customer is now at version 1, verify in Aggregates table
         }
     }
 }
